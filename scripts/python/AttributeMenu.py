6c303f0dd1b40024c244b14be30590bd1bc31565
zengchen <zengchen01@outlook.com>
2021-05-07 02:24:18 +0800

    210507

--- scripts/python/AttributeMenu.py
+++ scripts/python/AttributeMenu.py
@@ -156,6 +156,53 @@ def vertexVector(node_relative_path):
 
     return final_list
 
+def vertexFloatArray(node_relative_path):
+    node = hou.pwd()
+    input_node = node.node(node_relative_path)
+    geo = input_node.geometry()
+    final_list = []
+
+    if geo:
+        vertex_attrs_tuple = geo.vertexAttribs()
+        vertex_float_array_attrs_list = []
+
+        for vertex_attr in vertex_attrs_tuple:
+            if vertex_attr.size() == 1 and vertex_attr.dataType().name() == "Float" and vertex_attr.isArrayType():
+                name = vertex_attr.name()
+                vertex_float_array_attrs_list.append(name)
+
+        vertex_float_array_attrs_list.sort()
+
+        for attr_name in vertex_float_array_attrs_list:
+            final_list.append(attr_name)
+            final_list.append(attr_name)
+
+    return final_list
+
+
+def vertexIntArray(node_relative_path):
+    node = hou.pwd()
+    input_node = node.node(node_relative_path)
+    geo = input_node.geometry()
+    final_list = []
+
+    if geo:
+        vertex_attrs_tuple = geo.vertexAttribs()
+        vertex_int_array_attrs_list = []
+
+        for vertex_attr in vertex_attrs_tuple:
+            if vertex_attr.size() == 1 and vertex_attr.dataType().name() == "Int" and vertex_attr.isArrayType():
+                name = vertex_attr.name()
+                vertex_int_array_attrs_list.append(name)
+
+        vertex_int_array_attrs_list.sort()
+
+        for attr_name in vertex_int_array_attrs_list:
+            final_list.append(attr_name)
+            final_list.append(attr_name)
+
+    return final_list
+
 #############################################################################
 
 def pointFloat(node_relative_path):
@@ -293,6 +340,52 @@ def pointVector4(node_relative_path):
         point_vector4_attrs_list.sort()
 
         for attr_name in point_vector4_attrs_list:
+            final_list.append(attr_name)
+            final_list.append(attr_name)
+
+    return final_list
+
+def pointFloatArray(node_relative_path):
+    node = hou.pwd()
+    input_node = node.node(node_relative_path)
+    geo = input_node.geometry()
+    final_list = []
+
+    if geo:
+        point_attrs_tuple = geo.pointAttribs()
+        point_float_array_attrs_list = []
+
+        for point_attr in point_attrs_tuple:
+            if point_attr.size() == 1 and point_attr.dataType().name() == "Float" and point_attr.isArrayType():
+                name = point_attr.name()
+                point_float_array_attrs_list.append(name)
+
+        point_float_array_attrs_list.sort()
+
+        for attr_name in point_float_array_attrs_list:
+            final_list.append(attr_name)
+            final_list.append(attr_name)
+
+    return final_list
+
+def pointIntArray(node_relative_path):
+    node = hou.pwd()
+    input_node = node.node(node_relative_path)
+    geo = input_node.geometry()
+    final_list = []
+
+    if geo:
+        point_attrs_tuple = geo.pointAttribs()
+        point_int_array_attrs_list = []
+
+        for point_attr in point_attrs_tuple:
+            if point_attr.size() == 1 and point_attr.dataType().name() == "Int" and point_attr.isArrayType():
+                name = point_attr.name()
+                point_int_array_attrs_list.append(name)
+
+        point_int_array_attrs_list.sort()
+
+        for attr_name in point_int_array_attrs_list:
             final_list.append(attr_name)
             final_list.append(attr_name)
 
@@ -482,6 +575,29 @@ def primFloatArray(node_relative_path):
         prim_float_array_attrs_list.sort()
 
         for attr_name in prim_float_array_attrs_list:
+            final_list.append(attr_name)
+            final_list.append(attr_name)
+
+    return final_list
+
+def primIntArray(node_relative_path):
+    node = hou.pwd()
+    input_node = node.node(node_relative_path)
+    geo = input_node.geometry()
+    final_list = []
+
+    if geo:
+        prim_attrs_tuple = geo.primAttribs()
+        prim_int_attrs_list = []
+
+        for prim_attr in prim_attrs_tuple:
+            if prim_attr.size() == 1 and prim_attr.dataType().name() == "Int" and prim_attr.isArrayType():
+                name = prim_attr.name()
+                prim_int_attrs_list.append(name)
+
+        prim_int_attrs_list.sort()
+
+        for attr_name in prim_int_attrs_list:
             final_list.append(attr_name)
             final_list.append(attr_name)
 
